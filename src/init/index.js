dcb4135116c6233a60fc49ae1ffadb149f8de075
Dongkyun Kim <donkim0122@gmail.com>
2024-06-27 15:30:16 +0900

    Update: load proto files on initialization

--- src/init/index.js
+++ src/init/index.js
@@ -1,8 +1,10 @@
 import { loadGameAssets } from "./assets.js";
+import { loadProtos } from "./loadProtos.js";
 
 const initServer = async () => {
   try {
     await loadGameAssets();
+    await loadProtos();
   } catch (e) {
     console.error(e);
     // TODO: 에러 로그 처리
