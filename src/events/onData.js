a85a7029605e2ae642b70579015ca55afb8b7dd8
Dongkyun Kim <donkim0122@gmail.com>
2024-06-27 14:44:24 +0900

    Update: onData that appends & handles socket's byte stream data

--- src/events/onData.js
+++ src/events/onData.js
@@ -1,4 +1,23 @@
+import { config } from "../config/config.js";
+
+// curring
 export const onData = (socket) => (data) => {
-  // curring
-  console.log(data);
+  // data received as chunk, keep concatting data to the socket's buffer
+  socket.buffer = Buffer.concat([socket.buffer, data]);
+  const totalHeaderLength = config.packet.totalLength + config.packet.typeLength;
+  while (socket.buffer.length >= totalHeaderLength) {
+    const length = socket.buffer.readInt32BE(0);
+    const packetType = socket.buffer.readInt8(config.packet.totalLength);
+
+    if (socket.buffer.length < length) {
+      // 아직 패킷이 다 도착하지 않음
+      break;
+    }
+
+    const packet = socket.buffer.subarray(totalHeaderLength, length);
+    socket.buffer = socket.buffer.subarray(length);
+
+    console.log(`length: ${length}  pakcetType: ${packetType}`);
+    console.log(`packet: ${packet}`);
+  }
 };
