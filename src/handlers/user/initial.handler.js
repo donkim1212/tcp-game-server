ecc6b58ab7e8667dbe10f3d6bab1d298a4cf08e9
Dongkyun Kim <donkim0122@gmail.com>
2024-06-28 15:04:40 +0900

    Update: build response object to be used later

--- src/handlers/user/initial.handler.js
+++ src/handlers/user/initial.handler.js
@@ -1,9 +1,13 @@
+import { HANDLER_IDS, RESPONSE_SUCCESS_CODE } from "../../constants/handlerIds.js";
+import { createResponse } from "../../lib/utils/response/createResponse.js";
 import { addUser } from "../../session/user.session.js";
 
 const initialHandler = async ({ socket, userId, payload }) => {
   const { deviceId } = payload;
 
   addUser(socket, deviceId);
+
+  const initialResponse = createResponse(HANDLER_IDS.INITIAL, deviceId, RESPONSE_SUCCESS_CODE, { userId: deviceId });
 
   // 뭔가 처리가 끝났을 때 보내는 것
   socket.write("");
