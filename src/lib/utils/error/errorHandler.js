068dfe1b6f8211b3b15483f91c829d05f84d5bf6
Dongkyun Kim <donkim0122@gmail.com>
2024-06-28 15:13:11 +0900

    Add: error related files / error handling

---
+++ src/lib/utils/error/errorHandler.js
@@ -0,0 +1,21 @@
+import { createResponse } from "../response/createResponse";
+import { ErrorCodes } from "./errorCodes.js";
+
+export const handleError = (socket, error) => {
+  let responseCode;
+  let message;
+  console.error(error);
+
+  if (error.code) {
+    responseCode = error.code;
+    message = error.message;
+    console.error(`에러코드: ${error.code}, 메세지: ${error.message}`);
+  } else {
+    responseCode = ErrorCodes.SOCKET_ERROR;
+    message = error.message;
+    console.Error(`일반에러: ${error.message}`);
+  }
+
+  const errorResponse = createResponse(-1, null, responseCode, { message });
+  socket.write(errorResponse);
+};
